6299b49721891bac2bdeb219982c56e686ed61e2
mentoc2020 <dlaguddh1@gmail.com>
2024-11-23 21:31:07 +0900

    배포환경 수정

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import app from "./server"
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import cors from "cors";
 import { logger } from "./middlewares/middleware";
@@ -5,20 +6,13 @@ import { healthCkController, rootController } from "./controllers/baseController
 import { createTokenController } from "./controllers/tokenControllers";
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(logger);
-
 app.get('/', rootController)
 app.get('/health', healthCkController);
 app.post('/getToken', createTokenController);
 
-const handleListening = () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-};
-
-app.listen(PORT, handleListening);
+export default app;
