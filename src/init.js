29f33b61e032c58bdfa81053cbec4be22c93e418
TwotheMoon <dlaguddh1@gmail.com>
2024-12-07 17:43:48 +0900

    test2

--- src/init.js
+++ src/init.js
@@ -3,14 +3,21 @@ import "./models/NaverAllCate";
 import "./models/MyCate";
 import "./models/WhiteList";
 import app from "./server"
+
+
 import fs from "fs";
 import https from "https";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 433;
 
 const options = {
-  key: fs.readFileSync('./keys/private.pem'),
-  cert: fs.readFileSync('./keys/public.pem'),
+  key: fs.readFileSync(path.join(__dirname, './keys/private.pem')),
+  cert: fs.readFileSync(path.join(__dirname, './keys/public.pem')),
 }
 
 const handleListening = () => {
