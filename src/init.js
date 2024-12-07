6847e99d792ddcbbfe58186dc05c569047c5cb43
TwotheMoon <dlaguddh1@gmail.com>
2024-12-07 18:24:22 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -4,19 +4,10 @@ import "./models/MyCate";
 import "./models/WhiteList";
 import app from "./server"
 
-import fs from "fs";
-import https from "https";
-import path from "path";
-
-const PORT = 433;
-
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './keys/private.pem')),
-  cert: fs.readFileSync(path.join(__dirname, './keys/public.pem')),
-}
+const PORT = 4000;
 
 const handleListening = () => {
-  console.log(`Server is running on https://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 };
 
-https.createServer(options, app).listen(PORT, handleListening);
+app.listen(PORT, handleListening);
